9524595e229d5c3903af29d3c0c14268cc3abae2
becca4011 <becca4011@naver.com>
2021-04-29 18:57:40 +0900

    닷넷

---
+++ MDI binary/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2021_중간
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void 열기ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog op = new OpenFileDialog();
+            if (op.ShowDialog() == DialogResult.OK)
+            {
+                Image I = Image.FromFile(op.FileName);
+
+                Form2 Child = new Form2();
+                Child.image = I;
+                Child.MdiParent = this;
+                Child.Show();
+            }
+        }
+
+        private void 저장ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.FileName = "";
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                Form2 Child = ActiveMdiChild as Form2; // 저장하려는 Form의 인스턴스 얻어옴
+                if (Child != null)
+                {
+                    switch (saveFileDialog1.FilterIndex) // 파일 형식
+                    {
+                        case 1:
+                            Child.image.Save(saveFileDialog1.FileName, System.Drawing.Imaging.ImageFormat.Jpeg);
+                            break;
+                        case 2:
+                            Child.image.Save(saveFileDialog1.FileName, System.Drawing.Imaging.ImageFormat.Bmp);
+                            break;
+                        case 3:
+                            Child.image.Save(saveFileDialog1.FileName, System.Drawing.Imaging.ImageFormat.Png);
+                            break;
+                    }
+                }
+            }
+        }
+
+        private void 대화상자ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form2 original = ActiveMdiChild as Form2;
+
+            if (original != null)
+            {
+                Form3 dig = new Form3();
+                Image I = original.image;
+                dig.image = I;
+
+                if (dig.ShowDialog() == DialogResult.OK)
+                {
+                    Form2 Child = new Form2();
+                    Child.image = dig.newImage; // 이미지 저장한 것을 그대로 사용
+                    Child.MdiParent = this;
+                    Child.Show(); // 밝기 조절한 이미지
+                }
+            }
+        }
+    }
+}
---
+++ MDI binary/Form3.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2021_중간
+{
+    public partial class Form3 : Form
+    {
+        public int scroll;
+
+        public Form3()
+        {
+            InitializeComponent();
+            scroll = hScrollBar1.Value;
+            label4.Text = scroll.ToString();
+        }
+
+        public Image image { get; set; }
+        public Image bwImage { get; set; }
+        public Image newImage;
+
+        private void hScrollBar1_ValueChanged(object sender, EventArgs e)
+        {
+            scroll = hScrollBar1.Value;
+            label4.Text = scroll.ToString();
+            label2.Invalidate();
+        }
+
+        private void label1_Paint(object sender, PaintEventArgs e)
+        {
+            Bitmap B = new Bitmap(image);
+
+            if (B.PixelFormat.ToString() != "Format8bppIndexed") // 흑백 이미지가 아닐 경우 (컬러 이미지인 경우) / Format8bppIndexed : 흑백
+            {
+                // 흑백 이미지 만들기
+                for (int i = 0; i < B.Height; i++)
+                {
+                    for (int j = 0; j < B.Width; j++)
+                    {
+                        Color color = B.GetPixel(j, i);
+                        int c = (color.R + color.G + color.B) / 3;
+                        B.SetPixel(j, i, Color.FromArgb(c, c, c));
+                    }
+                }
+            }
+
+            bwImage = B;
+            e.Graphics.DrawImage(bwImage, 0, 0, label1.Width, label1.Height);
+        }
+
+        private void label2_Paint(object sender, PaintEventArgs e)
+        {
+            Bitmap B = new Bitmap(image);
+
+            for (int i = 0; i < B.Height; i++)
+            {
+                for (int j = 0; j < B.Width; j++)
+                {
+                    Color color = B.GetPixel(j, i);
+                    int c = color.R > scroll ? 255 : 0;
+                    B.SetPixel(j, i, Color.FromArgb(c, c, c));
+                }
+            }
+            newImage = B;
+            e.Graphics.DrawImage(newImage, 0, 0, label2.Width, label2.Height);
+        }
+    }
+}
